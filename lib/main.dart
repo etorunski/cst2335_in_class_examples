598c949b201360c0614ca3462b27a3dece6c62b4
Eric Torunski <etorunski@gmail.com>
2026-06-02 17:09:40 -0400

    week 4 start

--- lib/main.dart
+++ lib/main.dart
@@ -70,70 +70,10 @@ class _MyHomePageState extends State<MyHomePage> {
               .inversePrimary,
           title: Text("Favorite Recipes"),
         ),
-        drawer: Column(mainAxisAlignment: .end,
-          children: [
-            FilledButton(onPressed: () {}, child: Text("Go back")),
-          ],),
-        bottomNavigationBar: BottomNavigationBar(
-            onTap: (whichButton) {
-              if (whichButton == 0) {
-
-              }
-                ///camera {}
-              else if (whichButton == 1) {
-                //user clicked phone
-              }
-            }, //index of array item clicked
-            items: [
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.camera_alt_outlined), label: "Camera"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.call_end_outlined), label: "Phone"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.money_off_csred_outlined), label: "Cash"),
-
-            ]),
         body: Center(
             child:
             Row(children: [
-              Spacer(flex: 1),
-              Expanded(flex: 5,
-                  child:
-                  Column(
-                    crossAxisAlignment: .start,
-                    mainAxisAlignment: .spaceEvenly,
-                    children: [
-                      Expanded(flex: 1, child: Text(""),),
-
-                      Text("One-pan skillet cookie", style: TextStyle(
-                          color: Colors.orange, fontSize: 40.0),),
-                      Text("Ingredient list"),
-
-
-                      Expanded(flex: 1, child: Text(""),),
-
-
-                      Row(mainAxisAlignment: .start, children: [
-                        Icon(Icons.star),
-                        Text("1 stick of unsalted butter",
-                            style: TextStyle(color: Colors.orange))
-                      ],),
-                      Row(mainAxisAlignment: .start, children: [
-                        Icon(Icons.star),
-                        Text("1/2 C granulated sugar")
-                      ],),
-                      Row(mainAxisAlignment: .start, children: [
-                        Icon(Icons.star),
-                        Text("1/2 C light brown sugar")
-                      ],),
-
-
-                      Expanded(flex: 5, child: Text(""),),
-
-                    ], //empty column
-                  )),
-
-              Spacer(flex: 1),
+              OutlinedButton(child:Text("Click me"), onPressed: () { },)
             ]
             )
         )
