9bbad019a3abf75f26b2bd18bcc80f39bb47caeb
etorunski <etorunski@gmail.com>
2026-07-28 09:13:51 -0400

    finished localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,62 @@
+import 'package:cst2335_in_class_examples/database/app_database.dart';
+import 'package:cst2335_in_class_examples/database/todo.dart';
+import 'package:cst2335_in_class_examples/database/todo_dao.dart';
 import 'package:flutter/material.dart';
-import 'database/app_database.dart';
-import 'database/todo.dart';
-import 'database/todo_dao.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'AppLocalizations.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
-
-class MyApp extends StatelessWidget {
+//means never changes
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  static void setLocale(BuildContext context, Locale newLocale) async {
+
+    //this retrieves the state
+    MyAppState? state = context.findAncestorStateOfType<MyAppState>();
+    state?.changeLanguage(newLocale);
+  }
+
+  @override
+  MyAppState createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  //app starts off with this language:
+  var _locale = Locale("en", "ca");//english from canada
+
+  void changeLanguage(Locale locale){
+    setState(() {
+      _locale = locale;  //this updates your language to the new one
+    });
+  }
+
+
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      supportedLocales: [   //list all the languages your app supports:
+        Locale("de"),
+        Locale("en", "CA")
+      ],
+      localizationsDelegates: const [   //copy and paste this, don't change it
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      locale: _locale, //starting language
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Todo Floor Database'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -34,211 +71,181 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  TodoDao? todoDao;
-  List<Todo> todos = [];
-  late TextEditingController _nameController;
-  late TextEditingController _quantityController;
+  List<Todo> list1 = [];
+  Todo? selectedItem = null;//nothing is selected
 
-  Todo? selectedItem  = null;
+  late TodoDao itemDAO;
 
+  var isChecked = false;
+  var myFontSize = 0.0;
+  late TextEditingController _controller; //late means promise to initialize it later
+
+  //can't be async, because it overrides from parent
   @override
   void initState() {
     super.initState();
-    _nameController = TextEditingController();
-    _quantityController = TextEditingController();
-    _initDatabase();
-  }
+    _controller = TextEditingController(); //doing your promise to initialize
 
-  Future<void> _initDatabase()  async {
-    Future.delayed(Duration.zero, () async{
+    //want to load any existing data into the arraylist
+    //to open the database:
 
-    //create the database on disk:
-    final database = await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+    $FloorAppDatabase.databaseBuilder('ItemFile.db').build()
+        .then( (database){
+      itemDAO = database.todoDao;
 
-    //get the DAO of the database
-    todoDao = database.todoDao;
-
-    //query all objects:
-    _refreshTodos();
-      //code to run later
-
-    }  );
-
-  }
-
-  Future<void> _refreshTodos() async {
-    if (todoDao != null) {
-      //querying all from database:
-      final result = await todoDao!.findAllTodos();
-      setState(() {
-        //setting our list for the listView:
-        todos = result;
+      //query all data:
+      itemDAO.findAllTodos().then( (listOfItems ) {
+        setState(() { //redraw the GUI
+          list1.addAll(listOfItems); //put the items in the list:
+        });
       });
-    }
+    }  );
   }
 
-  Future<void> _addTodo() async {
-    //make sure fields have something in them:
-    if (_nameController.text.isNotEmpty && _quantityController.text.isNotEmpty) {
-    //create an Entity object:
-      final todo = Todo(
-        name: _nameController.text,
-        quantity: _quantityController.text,
-      );
-
-      //insert into database:
-      await todoDao?.insertTodo(todo);
-
-      //clear the textfields:
-      _nameController.clear();
-      _quantityController.clear();
-
-      //reload the data:
-      _refreshTodos();
-    }
-  }
-
-  Future<void> _deleteTodo(Todo todo) async {
-    //delete from database:
-    await todoDao?.deleteTodo(todo);
-
-    //requery data:
-    _refreshTodos();
-  }
-
+  //you are being removed
   @override
   void dispose() {
-    _nameController.dispose();
-    _quantityController.dispose();
     super.dispose();
+    //free memory:
+    _controller.dispose();
   }
 
-  Widget reactiveLayout(BuildContext bc)
-  {
-    var size = MediaQuery.of(context).size; //get the size of the screen
-    var height = size.height;
-    var width = size.width;
 
-    if((width > height) && (width > 720))//landscape / desktop mode
-        {
-        return Row(children: [
-         Expanded(child: listPage(bc), flex: 1), //1 / 4
-         Expanded(child:detailsPage(bc), flex:3) //1 / 4
-        ],);
-    }
-    else //it's portrait mode
-    {
-      if(selectedItem == null){
-        //nothing is selected:
-        return listPage(bc);
-      }
-      else{
-        return detailsPage(bc);
-      }
-    }
-  }
-
-  Widget detailsPage(BuildContext context){
-
-    if(selectedItem != null)
-      {
-        //there is a selected Item:
-        return Center(child:Column(
-            mainAxisAlignment: .center,
-            children:[
-          Text("Item name is: ${selectedItem!.name}"),
-          Text("Quantity is: ${selectedItem!.quantity}"),
-              OutlinedButton(child:Text("clear"), onPressed: (){
-                setState(() {
-                  selectedItem = null; // need to go back to the listview
-                });
-
-              },),
-              OutlinedButton(child:Text("Delete"), onPressed:(){
-                setState(() {
-
-                  todos.remove(selectedItem);//we don't have the index
-                  selectedItem = null;
-
-                });
-              })
-        ]));
-      }
-    else{
-      //nothing is selected
-      return Center(child:Column(children:[
-        Text("Select an item from the list to see the details")
-      ]));
-    }
-  }
-
-  Widget listPage(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _nameController,
-                      decoration: const InputDecoration(hintText: "Enter name"),
-                    ),
-                  ),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: TextField(
-                      controller: _quantityController,
-                      decoration: const InputDecoration(hintText: "Enter quantity"),
-                      keyboardType: TextInputType.number,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 8),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: _addTodo,
-                  child: const Text("add"),
-                ),
-              ),
-            ],
-          ),
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: todos.length,
-            itemBuilder: (context, index) {
-              final todo = todos[index];
-              return ListTile(
-                title: Text("${todo.name} (${todo.quantity})"),
-                onTap: () {
-                  //The user has selected this item:
-                  setState(() {
-                    //redraw the GUI:
-
-                    selectedItem = todo; //store what was selected
-                    });
-                  },
-              );
-            },
-          ),
-        ),
-      ],
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
+
+
+
+
     return Scaffold(
+
       appBar: AppBar(
+        actions: [
+          FilledButton(onPressed: (){MyApp.setLocale(context, Locale("en", "CA")); },
+              child: Text("English")),
+          FilledButton(onPressed: (){MyApp.setLocale(context, Locale("de")); },
+              child: Text("German")),
+          FilledButton(onPressed: (){MyApp.setLocale(context, Locale("ar")); },
+              child: Text("Arabic")),
+        ],
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: reactiveLayout(context),
+      body: reactiveLayout(), //decides how to lay out
+
     );
+  }
+
+  Widget reactiveLayout(){
+
+    var size = MediaQuery.of(context).size; ///how big is the screen?
+    var height = size.height;
+    var width = size.width;
+
+
+
+    if( (width>height) && (width > 720)) {
+      //tablet
+      return Row( children:[
+        Expanded(child: ListPage(),    flex:2), //Left side 40%
+        Expanded(child: DetailsPage(), flex:3) //Right side, 60%
+      ]);
+    }
+    else{ //Portrait mode / Phone
+      if( selectedItem== null)
+        return ListPage(); //show the list
+      else
+        return DetailsPage(); //show the details
+    }
+  }
+
+  Widget DetailsPage() {
+    if(selectedItem != null){
+      return Center(child:Column( children: [
+        Text("Name: ${selectedItem!.name}", style: TextStyle(fontSize: 40.0),),
+        Text("Quantity: ${selectedItem!.quantity}", style: TextStyle(fontSize: 40.0)),
+        Spacer(),//balloon that expands to fill the space
+        OutlinedButton(onPressed: (){
+          setState(() { selectedItem = null; });
+        }, child: Text("Delete")),
+
+
+        OutlinedButton(onPressed: (){
+          setState(() { selectedItem = null; });
+        }, child: Text("Close"))
+
+      ], mainAxisAlignment: MainAxisAlignment.center,)
+      ); //show what's been selected
+    }
+    else{
+      return Text(AppLocalizations.of(context)!.translate('TextSelect')!,style: TextStyle(fontSize: 30.0));
+    }
+  }
+
+  Widget ListPage()
+  {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row( mainAxisAlignment: MainAxisAlignment.spaceBetween, children:[
+            Flexible(
+                flex:1,
+                child: ElevatedButton( child:Text(AppLocalizations.of(context)!.translate('Add')!), onPressed:() {
+                  setState(() {
+                    //Unique IDs
+                    Todo newItem = Todo( name:_controller.value.text, quantity: "qty");
+
+                    itemDAO.insertTodo(newItem);//insert to database
+                    list1.add(newItem);
+                    _controller.text = "";
+                  });
+                } )
+
+            ),
+
+            Flexible( flex:4, child:TextField(controller: _controller ))
+          ]),
+
+          Expanded(child:
+          ListView.builder(
+              itemCount: list1.length,
+              itemBuilder:(context, rowNum) =>
+                  GestureDetector(child:Text("Row $rowNum, Name: ${list1[rowNum].name} Quantity: ${list1[rowNum].quantity }") ,
+
+                      onTap: () {
+                        setState(() {  selectedItem = list1[rowNum]; });
+                      },
+
+
+                      onLongPress: () {
+                        showDialog<String>(
+                            context: context,
+                            builder: (BuildContext context) => AlertDialog(
+                              title: const Text('Delete this?'),
+                              content: const Text('are you sure?'),
+                              actions: <Widget>[
+                                FilledButton(child:Text("Yes"), onPressed:() {
+                                  setState(() {
+                                    list1.removeAt(rowNum);
+                                  });
+
+                                  Navigator.pop(context);
+                                }),
+                                FilledButton(child:Text("Cancel"), onPressed:() {
+                                  Navigator.pop(context);
+
+                                }),
+                              ],
+                            )
+                        );
+
+                      })
+          )
+          )
+        ]);
   }
 }
